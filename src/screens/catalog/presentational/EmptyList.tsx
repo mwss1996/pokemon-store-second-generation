de260cbbd96ed836e47f5b47e995aea56712ba6d
Michael Wallace <mwss1996@gmail.com>
2020-07-25 19:42:25 +0200

    Update on EmptyList.tsx

--- src/screens/catalog/presentational/EmptyList.tsx
+++ src/screens/catalog/presentational/EmptyList.tsx
@@ -3,7 +3,12 @@ import { Styles } from "../../shared/Styles";
 import { defaultStyles } from "../../shared/defaultStyles";
 
 const styles: Styles = {
-	container: {}
+	container: {
+		display: "flex",
+		flexGrow: 1,
+		justifyContent: "center",
+		alignItems: "center"
+	}
 };
 export function EmptyList() {
 	return <div style={styles.container}>Sorry, there is nothing to show.</div>;
